ef4acf38459516032266b1e83593e8d01b401ab1
cyrustang <cyrus@cyrustang.com>
2024-10-17 03:12:47 +0800

    smaller mobile margins

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { AssetManagement } from '@/components/asset-management'
 
 export default function Home() {
-  return (
-    <div className="container mx-auto p-4">
-      <AssetManagement />
-    </div>
-  )
+  return <AssetManagement />
 }
